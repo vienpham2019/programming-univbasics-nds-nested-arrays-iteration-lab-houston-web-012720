00959340cc2aa7e04dbc848428618a953f86e3c8
vien pham <vienpham2019@gmail.com>
2019-12-10 05:58:37 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -42,7 +42,6 @@ def total_even_pairs(src)
   while row_index < src.count do
     if src[row_index][0] % 2 == 0 && src[row_index][1] % 2 == 0
       total += (src[row_index][0] + src[row_index][1])
-      row_index += 1
     end
     row_index += 1
   end
