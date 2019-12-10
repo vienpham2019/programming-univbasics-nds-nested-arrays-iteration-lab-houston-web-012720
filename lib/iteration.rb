febf4bbd73c22af86c73bef79b6b46d343f8e8e2
vien pham <vienpham2019@gmail.com>
2019-12-10 05:50:31 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -42,7 +42,7 @@ def total_even_pairs(src)
   while row_index < src.count do
     element_index = 0
     while element_index < src[row_index].count do
-      if src[row_index][element_index] % 2 = 0
+      if src[row_index][element_index] % 2 == 0
         total += src[row_index][element_index]
       end
       element_index += 1
